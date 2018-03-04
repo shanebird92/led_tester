4f871e311eea54872ac25da4231d2b313b61f260
Shane Bird <shane.bird92@ucdconnect.ie>
2018-03-04 21:04:16 +0000

    Have updated command line input to accomodate the instruction set

--- led_tester/cli.py
+++ led_tester/cli.py
@@ -6,11 +6,23 @@ import click
 
 
 @click.command()
-def main(args=None):
+@click.option("--input", default=None, help="input URI (file or URL)")
+def main(input=None):
     """Console script for led_tester."""
-    click.echo("Replace this message by putting your code into "
-               "led_tester.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    print("input", input)
+    
+    ''' takes two arguments from input as N and instructions, N is first number which I will use to make the
+    led, i.e will be the size of the led board squared. Instructions will then be each line which will be 
+    passed to the reg ex expression to have infor extracted.
+    ''' 
+    N, instructions = parseFile(input)
+    
+    ledTester = LEDtester(N)
+
+    for instruction in instructions:
+        ledTester.apply(instructions)
+
+    print ("The amount of LED's currently "ON" is: ", ledTester.countOccuied())
     return 0
 
 
