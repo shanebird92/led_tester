a9acc53f0576d77ed8fdd5b6550d839a12a02178
Shane Bird <shane.bird92@ucdconnect.ie>
2018-03-09 12:33:13 +0000

    accounted for files which go over array size and files which have incompatible commands

--- led_tester/cli.py
+++ led_tester/cli.py
@@ -13,12 +13,21 @@ def apply_led(N, instructions):
     
     for line in instructions:
         searchObj = re.search(".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*", line)
+        if searchObj:
+            cmd = searchObj.group(1)
+            x1 = int(searchObj.group(2))
+            y1 = int(searchObj.group(3))
+            x2 = int(searchObj.group(4))
+            y2 = int(searchObj.group(5))
         
-        cmd = searchObj.group(1)
-        x1 = int(searchObj.group(2))
-        y1 = int(searchObj.group(3))
-        x2 = int(searchObj.group(4))
-        y2 = int(searchObj.group(5))
+        if x1 < 0:
+            x1=0
+        if y1 < 0:
+            y1 =0
+        if x2 > N-1:
+            x2 = N-1
+        if y2 > N-1:
+            y2 =N-1    
     
     
         if cmd == "turn on":
@@ -38,8 +47,9 @@ def apply_led(N, instructions):
                         list2D[i][j]=0
                     elif list2D[i][j]==0:
                         list2D[i][j]=1
+        else:
+            continue
        
-        
     return sum(sum(x) for x in list2D)
 
 
